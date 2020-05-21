0b9fedcd74812c016528ce18d19f67a1ce18ab27
Sicong Huang <hugohuang1997@gmail.com>
2020-05-21 16:35:17 -0700

    initial commit

---
+++ article.py
@@ -0,0 +1,31 @@
+import re
+
+class Article:
+    def __init__(self, record, nlp):
+        self.title = record.find('./titles/title').text
+        self.authors = [author.text for author in record.findall('./contributors/authors/author')]
+        self.keywords = [keyword.text for keyword in record.findall('./keywords/keyword')]
+        self.abstract = re.sub(r'<.*?>', '', record.find('./abstract').text)
+        self.parse = nlp(self.abstract)
+        doi = record.find('./electronic-resource-num')
+        self.doi = doi.text if doi != None else ''
+
+    def tokens(self, lower=False, no_punct=True, no_stop=True):
+        token_list = []
+        for token in self.parse:
+            if token.is_punct and no_punct:  # skip punctuation when needed
+                continue
+            if token.is_stop and no_stop:  # skip stop words when needed
+                continue
+            if token.is_space:  # always skip white space
+                continue
+            if lower:
+                token_list.append(token.text.lower())
+            else:
+                token_list.append(token.text)
+        return token_list
+
+    def __str__(self):
+        s = 'Title: {}\nAuthors: {}\nAbstract: {}\nDOI: {}'.format(self.title, \
+            ', '.join(self.authors), self.abstract, self.doi)
+        return s
